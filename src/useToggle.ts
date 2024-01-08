ba86e02e380618aa0e34ac21591b0b281b6c2452
chengbo.chu <13951848647@163.com>
2024-01-08 15:02:49 +0800

    feat: implement useToggle function

--- src/useToggle.ts
+++ src/useToggle.ts
@@ -1,5 +1,32 @@
 import * as React from "react";
+import { useState } from 'react';
 
-export default function useToggle() {
-  return [true, null];
+export default function useToggle(initialValue?: boolean) {
+  // 1.useToggle should correctly establish its initial value, casting it to a boolean if it's not one
+  const [value, setValue] = useState(!!initialValue);
+
+  const toggle = (newValue?: boolean) => {
+    // 2.useToggle should toggle the state when its updater function is invoked without a value
+    if (newValue === undefined) {
+      setValue(!value);
+      return;
+    }
+    // 3.seToggle should set the state to the provided value when its updater function is called with a boolean value
+    if (typeof newValue === "boolean") {
+      setValue(newValue);
+      return;
+    }
+    // 4.useToggle should not change the state when its updater function is called with the same boolean value
+    if (value === newValue) {
+      return;
+    }
+
+    // 5.seToggle should toggle the state when its updater function is called with a value that isn't a boolean
+    if (typeof newValue !== "boolean") {
+      setValue(!value);
+      return;
+    }
+  };
+
+  return [value, toggle];
 }
